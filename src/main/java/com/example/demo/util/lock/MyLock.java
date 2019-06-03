032c19b86822fa88921464f75128f39610f0089e
mengli <mengli@yxholding.com>
2019-06-03 09:55:13 +0800

    锁

---
+++ src/main/java/com/example/demo/util/lock/MyLock.java
@@ -0,0 +1,96 @@
+package com.example.demo.util.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author limeng
+ * @Description 可重入锁
+ * @Date @Date: 2019/5/24 10:16
+ * @Modified by :
+ **/
+public class MyLock {
+
+    private static final int acquire = 1;
+
+    private final MySync sync;
+
+    public  MyLock() {
+        this.sync = new MySync();
+    }
+
+    public static void main(String[] args) {
+        new ReentrantLock().lock();
+    }
+
+    /**
+     * 解锁
+     */
+    public void unlock() {
+        sync.unlock();
+    }
+
+    static class MySync extends MyAbstractQueueSync {
+        void lock() {
+            // 尝试修改state 0表示为占用
+            if (!tryAcquire()) {
+                setQueue();
+                // 如果未成功 阻塞线程
+                System.out.println(Thread.currentThread() + "==锁已被占用");
+                park();
+            }
+        }
+
+        boolean tryAcquire() {
+            Thread thread = Thread.currentThread();
+            // 先判断state是否0
+            int state = getState();
+            if (state == 0) {
+                // 未有线程获得锁  cas state
+                if (compareAndSetState(0, MyLock.acquire)) {
+                    setInclusiveThread(thread);
+                    System.out.println(Thread.currentThread() +"获得了锁， state ：" + getState());
+                    return true;
+                }
+            } else {
+                // 已经有人获取锁，判断是否是本人 可重入
+                if (thread == getInclusiveThread()) {
+                    setState(state + MyLock.acquire);
+                    System.out.println(Thread.currentThread() +"获得了锁， state ：" + getState());
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void unlock() {
+            if (tryRelease()) {
+                // 唤醒队列头
+                unPark();
+            }
+        }
+
+        boolean tryRelease() {
+            Thread thread = Thread.currentThread();
+            if (thread != getInclusiveThread()) {
+                System.out.println("释放锁失败， 线程不匹配！");
+                return false;
+            }
+            // 先判断state是否0
+            int newState = getState() - MyLock.acquire;
+            if (newState < 0) {
+                System.out.println("释放锁失败， state 不匹配！");
+                return false;
+            }
+            if (newState == 0) {
+                setInclusiveThread(null);
+                return true;
+            }
+            setState(newState);
+            return false;
+        }
+    }
+
+    public void lock() {
+        sync.lock();
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,10 +3,16 @@ package com.example.demo;
 import com.example.demo.ml.bean.Article;
 import com.example.demo.ml.bean.Person;
 import com.example.demo.ml.impl.DefaultWeChatService;
+import com.example.demo.util.lock.MyAbstractQueueSync;
+import com.example.demo.util.lock.MyLock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +32,51 @@ public class DemoApplicationTests {
 		weChat.publish(new Article("小安康居然。。。"));
 		// 王者登录
 		new Person("孟立", 24).login(weChat);
+	}
+
+	/**
+	 * lock
+	 */
+	@Test
+	public void testLock() {
+		//MyLock lock = new MyLock();
+		ReentrantLock lock = new ReentrantLock();
+		Thread aT = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					lock.lock();
+					System.out.println("a");
+					try {
+						Thread.sleep(5000L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}  finally {
+					lock.unlock();
+				}
+			}
+		});
+		Thread bT = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					lock.lock();
+					try {
+						Thread.sleep(5000L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("b");
+				} finally {
+					lock.unlock();
+				}
+			}
+		});
+		aT.start();
+		bT.start();
+		System.out.println();
+
 	}
 
 }
